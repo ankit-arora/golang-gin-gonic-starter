1da45b40bed42334665df3fcd253af4242b43fc6
Ankit Arora <ankit.j.arora@gmail.com>
2024-05-06 15:28:23 +0530

    update with version

--- foo/main.go
+++ foo/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+var version string
 
 func main() {
+	log.Println("Starting version", version)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
